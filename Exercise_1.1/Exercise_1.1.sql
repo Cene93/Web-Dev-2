c0f1eb8f6ab8babcf0a4f148252b31c09358636f
CENE-LAPTOP\Cene <cenedem@gmail.com>
2018-01-26 17:21:03 +0100

    no message

---
+++ Exercise_1.1/Exercise_1.1.sql
@@ -0,0 +1,10 @@
+SELECT Name FROM Artist;
+
+SELECT * FROM Invoice
+	WHERE BillingCountry = 'Germany';
+	
+Select Count(AlbumID) FROM Album;
+
+SELECT COUNT(Country) FROM Customer
+	WHERE Country = 'France';
+	
--- Exercise_2.1/Exercise_2.1.sql
+++ Exercise_2.1/Exercise_2.1.sql
@@ -1,29 +1,29 @@
---SELECT MAX(Total) FROM Invoice
+SELECT MAX(Total) FROM Invoice
 
---SELECT MIN(Total) FROM Invoice
+SELECT MIN(Total) FROM Invoice
 
---SELECT BillingCity, COUNT(*) as Num FROM Invoice
-	--GROUP BY BillingCity 
-	--ORDER BY Num DESC
+SELECT BillingCity, COUNT(*) as Num FROM Invoice
+    GROUP BY BillingCity 
+	ORDER BY Num DESC
 	
---SELECT COUNT(*) FROM Track
-	--JOIN MediaType ON MediaType.MediaTypeId = Track.MediaTypeId
-	--WHERE MediaType.Name = 'Protected AAC audio file'
+SELECT COUNT(*) FROM Track
+	JOIN MediaType ON MediaType.MediaTypeId = Track.MediaTypeId
+	WHERE MediaType.Name = 'Protected AAC audio file'
 	
---SELECT Artist.Name, COUNT(*) as Albums FROM Album
-	--JOIN Artist ON Artist.ArtistId = Album.ArtistId
-	--GROUP BY Album.ArtistId
-	--ORDER BY Albums DESC
+SELECT Artist.Name, COUNT(*) as Albums FROM Album
+	JOIN Artist ON Artist.ArtistId = Album.ArtistId
+	GROUP BY Album.ArtistId
+	ORDER BY Albums DESC
 	
---SELECT Genre.Name, COUNT(*) as Tracks FROM Track
-	--JOIN Genre ON Track.GenreId = Genre.GenreId
-	--GROUP BY Track.GenreId
-	--ORDER BY Tracks DESC
+SELECT Genre.Name, COUNT(*) as Tracks FROM Track
+	JOIN Genre ON Track.GenreId = Genre.GenreId
+	GROUP BY Track.GenreId
+	ORDER BY Tracks DESC
 	
---SELECT FirstName, LastName, Total FROM Customer
-	--JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
-	--GROUP BY Total
-	--ORDER BY Total DESC
+SELECT FirstName, LastName, Total FROM Customer
+	JOIN Invoice ON Customer.CustomerId = Invoice.CustomerId
+	GROUP BY Total
+	ORDER BY Total DESC
 	
 SELECT Track.Name, InvoiceId FROM InvoiceLine
 	JOIN Track ON Track.TrackId = InvoiceLine.TrackId
